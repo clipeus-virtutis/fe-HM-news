914dc3d0064f5dd2813a0dd4a95c96a57487feb7
clipeus-virtutis <henry_mcd_93@hotmail.com>
2022-03-10 14:07:18 +0000

    css stlying

--- src/components/TopicButtons.jsx
+++ src/components/TopicButtons.jsx
@@ -4,6 +4,7 @@ export default function TopicButtons(props) {
   const { topics } = props;
   return (
     <section className="TopicButtons-Section">
+      <h3>Filter articles by:</h3>
       {topics.map((topic, index) => {
         return (
           <Link to={`/articles/${topic}`} key={topic}>
